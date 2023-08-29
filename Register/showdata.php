b215c8b7814ccb07585463c47774776d76f04e60
SenpaiCha <54840053+SenpaiCha@users.noreply.github.com>
2023-08-29 23:21:09 +0700

    Add register work

---
+++ Register/showdata.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>showdata</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+<body>
+<?php
+echo "ชื่อผู้ทำแบบฟอร์ม :".$_POST["pp_name"]."<br>";
+echo "เพศผู้เข้าทำแบบฟอร์ม :".$_POST["pp_sex"]."<br>";
+echo "คำถามข้อที่ 1 ตอบ ".$_POST["pp_question1"]."<br>";
+
+?>
+</body>
+</html>
